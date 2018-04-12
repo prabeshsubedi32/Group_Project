b0a0fae093d9e3fe86cde8764e37c4f868b1e250
Sushil Kaphle <SUSPENS_2K@YAHOO.COM>
2018-04-12 17:22:39 -0400

    Just added Admin and vendor models

---
+++ DynamicVendors/DynamicVendors/Models/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicVendors.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        List<Vendor> vendors { get; set; }
+    }
+}
---
+++ DynamicVendors/DynamicVendors/Models/Vendor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicVendors.Models
+{
+    public class Vendor
+    {
+        public int VendorId { get; set; }
+        public string VendorName { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public int Id { get; set; }
+
+    }
+}
