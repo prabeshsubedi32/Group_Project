ecbe0ab1e14314d6b82a36f83a4c9e675382507b
Prabesh <Prabesh@PRABESH-PC>
2018-04-12 21:37:12 -0400

    Finished with the Admin CRUD operation

--- DynamicVendors/DynamicVendors/Global.asax.cs
+++ DynamicVendors/DynamicVendors/Global.asax.cs
@@ -18,6 +18,7 @@ namespace DynamicVendors
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            UnityMvcActivator.Start();
         }
     }
 }
---
+++ DynamicVendors/DynamicVendors/Models/DynamicDBContext.cs
@@ -0,0 +1,15 @@
+﻿using DynamicVendors.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DynamicVendors.Repository
+{
+    public class DynamicDBContext : DbContext
+    {
+        public DbSet<Admin> admin { get; set; }
+        public DbSet<Vendor> vendors { get; set; }
+    }
+}
