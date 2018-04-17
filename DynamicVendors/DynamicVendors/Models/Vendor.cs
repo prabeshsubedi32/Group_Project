9093c6e389be60c7a0420097ae510f277effd394
Sushil Kaphle <SUSPENS_2K@YAHOO.COM>
2018-04-17 10:36:32 -0400

    some changes in vender side....

--- DynamicVendors/DynamicVendors/Models/Vendor.cs
+++ DynamicVendors/DynamicVendors/Models/Vendor.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace DynamicVendors.Models
 {
+    [Table("Vendors")]
     public class Vendor
     {
         public int VendorId { get; set; }
         public string VendorName { get; set; }
         public string UserName { get; set; }
         public string UserPassword { get; set; }
-        public int Id { get; set; }
+        //public int Id { get; set; }
 
     }
 }
